b55ecb1bf71597aa14c7cb124e6a1743f384839f
asanchez412 <sanchezandres41997@gmail.com>
2020-04-16 20:10:55 -0300

    v2 - agrega GetTextToPrint

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -29,12 +29,11 @@ namespace Full_GRASP_And_SOLID.Library
         {
             string result = string.Empty;
             
-            //Console.WriteLine($"Receta de {this.FinalProduct.Description}:");
-            //foreach (Step step in this.steps)
-            //{
-            //    Console.WriteLine($"{step.Quantity} de '{step.Input.Description}' " +
-            //        $"usando '{step.Equipment.Description}' durante {step.Time}");
-            //}
+            result = $"Receta de {this.FinalProduct.Description}";
+            foreach (Step step in this.steps)
+            {
+                result = result + $"{step.Quantity} de '{step.Input.Description}' " + $"usando '{step.Equipment.Description}' durante {step.Time}";
+            }
             return result;
         }
     }
